060d25a985e2a337fcdfe43b3e336a6ad05df28a
Mezbahur Rahman <62571684+Mezbah-byte@users.noreply.github.com>
2022-07-26 09:11:34 +0600

    first

---
+++ lib/src/model/product_category.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerce_3/src/model/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductCategory {
+  ProductType type;
+  bool isSelected;
+  IconData icon;
+  ProductCategory(this.type, this.isSelected, this.icon);
+}
